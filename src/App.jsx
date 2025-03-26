e1207ba54ddcd7ec3c010bb62f04d9318b021990
Nasir <nasirmalek517@gmail.com>
2025-03-26 14:40:13 +0530

    something added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {TodoProvider} from './contexts'
 import './App.css'
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
+import AdsterraAd from './components/AdsterraAd'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -49,6 +50,7 @@ function App() {
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                  <AdsterraAd />
                     <div className="mb-4">
                         {/* Todo form goes here */} 
                         <TodoForm />
@@ -63,9 +65,13 @@ function App() {
                           </div>
                         ))}
                     </div>
+                      <AdsterraAd />
+
                 </div>
+        
             </div>
     </TodoProvider>
+    
   )
 }
 
