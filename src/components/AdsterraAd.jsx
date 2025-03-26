aaadb9d87480a2edb2a30e16ccbb3a91fd74b43c
Nasir Malek <113028970+nasirmalek@users.noreply.github.com>
2025-03-26 14:25:35 +0530

    Update AdsterraAd.jsx

--- src/components/AdsterraAd.jsx
+++ src/components/AdsterraAd.jsx
@@ -4,19 +4,19 @@ const AdsterraAd = () => {
   useEffect(() => {
     const script = document.createElement("script");
     script.type = "text/javascript";
+    script.src = "https://wormdistressedunit.com/d9tnr74r2?key=4dc6ce55eb631b5e1a77d3e90250a000"; // Use your actual Adsterra script URL
     script.async = true;
-    script.src = "https://wormdistressedunit.com/5892302f445b468a744a75ccaadc913b/invoke.js"; // Adsterra script
     document.body.appendChild(script);
 
     return () => {
-      document.body.removeChild(script); // Cleanup when unmounting
+      document.body.removeChild(script);
     };
   }, []);
 
   return (
-    <div style={{ textAlign: "center", margin: "20px 0" }}>
-      {/* Placeholder for the ad */}
-      <div id="ad-container"></div>
+    <div>
+      <p>Loading Ad...</p>
+      <div id="adsterra-container"></div> {/* The ad will be placed here */}
     </div>
   );
 };
